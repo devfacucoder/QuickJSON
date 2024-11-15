0d0e848e02909fba9174c3a5f3db8abe5921cf36
facundo <facudoer@gmail.com>
2024-11-15 12:25:18 -0300

    Add CORS middleware to allow cross-origin requests

--- index.js
+++ index.js
@@ -2,8 +2,16 @@ import express from "express";
 const app = express();
 
 import mongoose from "./db.js";
-import { createTexto, getTexto,getTextoById,createTextoForObjeto } from "./texto.ctrl.js";
-app.use(express.json())
+import cors from "cors";
+
+import {
+  createTexto,
+  getTexto,
+  getTextoById,
+  createTextoForObjeto,
+} from "./texto.ctrl.js";
+app.use(cors());
+app.use(express.json());
 app.get("/", getTexto);
 app.get("/:ide", getTextoById);
 
